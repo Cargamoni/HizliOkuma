2612204328131e5809737cdf36fba319efdc243f
cargamoni <ahmetcanirdem@hotmail.com>
2020-08-08 19:32:34 +0100

    Kayan Resimler

--- HizliOkuma/KayanResimlerForm.Designer.cs
+++ HizliOkuma/KayanResimlerForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace HizliOkuma
 {
-    partial class Form1
+    partial class KayanResimlerForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,35 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ResimHizi = new System.Windows.Forms.Timer(this.components);
+            this.Zamanlayici = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
-            // button1
+            // panel1
             // 
-            this.button1.Location = new System.Drawing.Point(713, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Alıştırma 1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 0;
             // 
-            // Form1
+            // ResimHizi
+            // 
+            this.ResimHizi.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Zamanlayici
+            // 
+            this.Zamanlayici.Tick += new System.EventHandler(this.Zamanlayici_Tick);
+            // 
+            // KayanResimlerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.Controls.Add(this.panel1);
+            this.Name = "KayanResimlerForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "KayanResimlerForm";
+            this.SizeChanged += new System.EventHandler(this.KayanResimlerForm_SizeChanged);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer ResimHizi;
+        private System.Windows.Forms.Timer Zamanlayici;
     }
 }
-
